b32aa5d32f3c417c619c1500b07a563eff102dc9
Djibril <idjibrilmaman@gmail.com>
2022-03-31 19:14:03 +0300

    project setup

---
+++ src/modules/theme.ts
@@ -0,0 +1,51 @@
+export const theme = {
+  colors: {
+    primary: '#b31f30',
+    primary50: '#d33749',
+    secondary: '#27CFC9',
+    secondary50: '#27cfc9bb',
+    black: '#000',
+    white: '#fff',
+  },
+  typography: {
+    header1: {
+      fontSize: 18,
+      fontWeight: '700',
+      lineHeight: 24,
+    },
+    header2: {
+      fontSize: 14,
+      fontWeight: '700',
+      lineHeight: 24,
+    },
+    body1: {
+      fontSize: 14,
+      fontWeight: '500',
+      lineHeight: 24,
+    },
+    body2: {
+      fontSize: 12,
+      fontWeight: '500',
+      lineHeight: 16,
+    },
+    placeholder: {
+      fontSize: 14,
+      fontWeight: '400',
+    },
+    button: {
+      fontSize: 14,
+      fontWeight: '500',
+      textTransform: 'uppercase',
+    },
+  },
+  spacing: {
+    xxs: 5,
+    xs: 10,
+    s: 20,
+    m: 30,
+    l: 40,
+    xl: 50,
+    xxl: 70,
+  },
+  radius: 4,
+};
